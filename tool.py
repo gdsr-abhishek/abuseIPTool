b40c0ff2ff8d693ff94ba47a541ef3adb9f32c88
Abhishek Guntuboina <gdsr.abhishek@gmail.com>
2025-02-28 15:41:05 +0530

    abuse IP tool V1

--- tool.py
+++ tool.py
@@ -1,13 +1,34 @@
 import requests
 from dotenv import load_dotenv
 import os
+from langchain_openai import ChatOpenAI
+import gradio as gr
+def getIPSummary(ipData):
+    key=os.getenv('OPENAI_API_KEY')
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key= key
+)
+    prompt = f"""
+Summarise abuse IP data and give a verdict on whether the IP address is malicious or not in 250 words:
+
+{ipData}
+"""
+    result =llm.invoke(prompt)
+    return result.content
 def getReputation(ip):
     load_dotenv()
     ABUSE_IP_API_KEY= os.getenv('ABUSE_IP_API_KEY')
     url="https://api.abuseipdb.com/api/v2/check"
     body={"ipAddress":ip}
     headers={"Key":ABUSE_IP_API_KEY,"Accept":'application/json'}
-    response =requests.get(url=url,params=body,headers=headers)
-    return response.text
+    response =requests.get(url=url,params=body,headers=headers).text
+    result = getIPSummary(response)
+    return result
 
-print(getReputation('92.255.85.107'))
+
+
+demo = gr.Interface(fn=getReputation, inputs="textbox", outputs="textbox")
+
+if __name__ == "__main__":
+    demo.launch()
